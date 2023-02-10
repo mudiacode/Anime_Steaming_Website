cd76b7b6b6998ee212ed2229abc0ffe7c32d84cb
MudiaUON <mudiauon@gmail.com>
2023-02-10 16:33:02 +0000

    Complete responsive navbar with js

---
+++ src/app.js
@@ -0,0 +1,17 @@
+// Hamburger Menu
+const hamburger_menu = document.querySelector("#hamburger");
+const hamburger_btn = document.querySelector("#hamburger-btn");
+
+// Search Toggle
+const search_menu = document.querySelector("#search-menu");
+const search_btn = document.querySelector("#search-btn");
+
+// Remove display none class on click (hamburger)
+hamburger_btn.addEventListener("click", (e) => {
+  hamburger_menu.classList.toggle("menu-toggle");
+});
+
+// Remove display none class on click (hamburger)
+search_btn.addEventListener("click", (e) => {
+  search_menu.classList.toggle("search-toggle");
+});
